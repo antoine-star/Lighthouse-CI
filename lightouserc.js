e1a5c76b76598f33151821fe2f1c7dc7946a313a
antoine-star <74658206+antoine-star@users.noreply.github.com>
2020-12-01 12:59:13 +0100

    Update lightouserc.js

--- lightouserc.js
+++ lightouserc.js
@@ -1,7 +1,12 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['https://www.darwinsocialnoise.com/'],
+      startServerCommand: 'rails server -e production',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
   },
 };
+
